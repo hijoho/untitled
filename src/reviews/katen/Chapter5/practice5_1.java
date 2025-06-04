ad3146cab6c3847cffb167b7e6cbf892d7bdad2b
241007 <241007@std.hi-joho.ac.jp>
2025-06-04 10:06:22 +0900

    加点問題　次は練習問題5から

---
+++ src/reviews/katen/Chapter5/practice5_1.java
@@ -0,0 +1,11 @@
+package reviews.katen.Chapter5;
+
+public class practice5_1 {
+    public static void main(String[] args) {
+        introduceOneself();
+    }
+
+    public static void introduceOneself(){
+
+    }
+}
